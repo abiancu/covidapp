6e354c4296d52b4fe645a39f064b4708edc443b1
abiancu <alejandrobiancucci@gmail.com>
2020-12-16 18:11:44 -0600

    init commit

---
+++ app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import path from 'path';
+
+// Using express
+const app = express();
+
+// Port number
+const PORT = 3000;
+
+// View engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
+
+// Locals
+app.locals.pageTitle = 'COVID19-HOME';
+
+// Serve static files
+app.use(express.static(path.join(__dirname, '/static')));
+
+// Home page
+app.get('/', (req, res) => {
+  res.render('home', { title: 'COVID19 Tracker' });
+});
+
+// Listening
+app.listen(PORT, console.log(`Running on port: ${PORT}`));
