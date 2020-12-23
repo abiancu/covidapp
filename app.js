0e436900754b73bd639b2315e074f57ff89757c4
abiancu <alejandrobiancucci@gmail.com>
2020-12-23 12:35:52 -0600

    simplier template

--- app.js
+++ app.js
@@ -13,9 +13,10 @@ app.set('views', path.join(__dirname, './views'));
 
 // Locals
 app.locals.pageTitle = 'COVID19-HOME';
-
+app.locals.covid_global = 'COVID-19 Global Cases';
 // Serve static files
 app.use(express.static(path.join(__dirname, '/static')));
+app.use(express.static(path.join(__dirname, '/static/img')));
 
 // Home page
 app.get('/', (req, res) => {
