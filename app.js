a9ff16c818914c223311b1110a8ff46a6f9f14f3
abiancu <alejandrobiancucci@gmail.com>
2020-12-27 11:36:22 -0600

    adding route folder

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import path from 'path';
+import bodyParser from 'body-parser';
 
 // Using express
 const app = express();
+const routes = express.Router();
 
 // Port number
 const PORT = 3000;
+
+// Request Body Parsers
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // View engine
 app.set('view engine', 'ejs');
@@ -14,14 +20,15 @@ app.set('views', path.join(__dirname, './views'));
 // Locals
 app.locals.pageTitle = 'COVID19-HOME';
 app.locals.covid_global = 'COVID-19 Global Cases';
+
+// Add Middleware
+
 // Serve static files
 app.use(express.static(path.join(__dirname, '/static')));
 app.use(express.static(path.join(__dirname, '/static/img')));
 
 // Home page
-app.get('/', (req, res) => {
-  res.render('home', { title: 'COVID19 Tracker' });
-});
+app.get('/', require('./routes/home'));
 
 // Listening
 app.listen(PORT, console.log(`Running on port: ${PORT}`));
