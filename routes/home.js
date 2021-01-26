d9ff3ed9041035891740873711b9feb21d30c0d4
abiancu <alejandrobiancucci@gmail.com>
2021-01-25 22:21:32 -0600

    code style change--eslint rules

--- routes/home.js
+++ routes/home.js
@@ -8,14 +8,17 @@ module.exports = () => {
     router.get('/', (req, res, next) => {
         try {
             data(
-                (
-                    err,
-                    { newConfirmed, totalConfirmed, totalDeaths, countryInfo }
-                ) => {
+                (err, {
+                    newConfirmed,
+                    totalConfirmed,
+                    totalDeaths,
+                    countryInfo
+                }) => {
+ 
                     // Console.log(newConfirmed, totalConfirmed, totalDeaths);
 
                     // Pagination
-                    const firstPage = 1
+                    const firstPage = 1;
                     const limit = 10;
                     const startIndex = (firstPage - 1) * limit; // Pages are  1 index while start index is zero based; so we need to subract one to get index[0]
                     const endIndex = firstPage * limit;
@@ -28,11 +31,7 @@ module.exports = () => {
                     const totalResults = countryInfo.length;
 
                     // Determining how many pages based on the limit
-                    for (
-                        let i = firstPage;
-                        i <= Math.ceil(totalResults / limit);
-                        i++
-                    ) {
+                    for (let i = firstPage; i <= Math.ceil(totalResults / limit); i++) {
                         pageNumbers.push(i);
                     }
                     // Console.log(pageNumbers);
