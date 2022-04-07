f4df4ece4def23b25fbe870aa0b3967453ff501d
Alejandro Biancucci <32583931+abiancu@users.noreply.github.com>
2022-04-06 21:39:19 -0500

    Delete secrets.js

--- secrets.js
+++
@@ -1,8 +0,0 @@
-
-const secrets = {
-    API_KEY: '14aadf98bcmsh669b27c9c93fedap1729e0jsndff0e502248d',
-    HOST_KEY: 'covid-193.p.rapidapi.com',
-    url: 'https://covid-193.p.rapidapi.com/statistics'
-};
-
-module.exports = secrets;
